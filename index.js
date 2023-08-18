91d8eeedb3939c9ab1e7bea66f6a9eb8b5d9ab66
arpit <arpit2003y@gmail.com>
2023-08-18 18:12:40 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,37 @@
+const puppeteer = require("puppeteer")
+
+
+const getQoutes = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    }
+    )
+
+    const page = await browser.newPage();
+
+    await page.goto("https://quotes.toscrape.com/",
+        {
+            waitUntil: "domcontentloaded",
+        }
+    )
+
+
+    const quotes = await page.evaluate(() => {
+
+        const quoteList = document.querySelectorAll(".quote")
+
+
+        return Array.from(quoteList).map((quote) => {
+            const text = quote.querySelector(".text").innerText
+            const author = quote.querySelector(".author").innerText
+            return { text, author }
+        })
+    })
+
+    console.log(quotes)
+
+    await browser.close();
+}
+
+getQoutes();
